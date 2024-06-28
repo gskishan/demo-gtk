6ead09d2aba9974c8b175887bd425a70e1fd77ca
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-06-28 14:49:10 +0530

    Update hooks.py

--- demoerp/hooks.py
+++ demoerp/hooks.py
@@ -123,7 +123,7 @@ app_license = "mit"
 # Hook on document methods and events
 doc_events = {
     "Lead": {
-        "validate": "demoerp.custom_script.lead.lead_validate"
+        "validate": "demoerp.custom_script.lead.lead.lead_validate"
     },
 }
 # doc_events = {
