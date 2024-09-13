8c0034013098773e39da69eaaedeefdfceebf54d
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-09-13 12:55:31 +0530

    Update payment_entry.py

--- demoerp/custom_script/payment_entry/payment_entry.py
+++ demoerp/custom_script/payment_entry/payment_entry.py
@@ -4,6 +4,36 @@ from erpnext.accounts.doctype.payment_entry.payment_entry import PaymentEntry
 from erpnext.accounts.doctype.payment_entry.payment_entry import *
 
 class CustomPaymentEntry(PaymentEntry):
+	def validate(self):
+		self.setup_party_account_field()
+		self.set_missing_values()
+		self.set_liability_account()
+		self.set_missing_ref_details(force=True)
+		self.validate_payment_type()
+		self.validate_party_details()
+		self.set_exchange_rate()
+		self.validate_mandatory()
+		frappe.errprint("1")
+		self.validate_reference_documents()
+		frappe.errprint("2")
+
+		self.set_amounts()
+		self.validate_amounts()
+		self.apply_taxes()
+		self.set_amounts_after_tax()
+		self.clear_unallocated_reference_document_rows()
+		self.validate_transaction_reference()
+		self.set_title()
+		self.set_remarks()
+		self.validate_duplicate_entry()
+		self.validate_payment_type_with_outstanding()
+		self.validate_allocated_amount()
+		self.validate_paid_invoices()
+		self.ensure_supplier_is_not_blocked()
+		self.set_tax_withholding()
+		self.set_status()
+		self.set_total_in_words()
+
 	def validate_reference_documents(self):
 		valid_reference_doctypes = self.get_valid_reference_doctypes()
 
